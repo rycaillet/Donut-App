b41403e2f911c603284cb1f74231e5fda7953577
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-10-20 10:05:27 -0400

    updated models/donut.js file

---
+++ models/donut.js
@@ -0,0 +1,13 @@
+const { Schema } = require('mongoose')
+
+const Donut = new Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: String, required: true },
+    review: { type: String, required: true },
+    donutShop_id: { type: Schema.Types.ObjectId, ref: 'donutShop_id' }
+  },
+  { timestamps: true }
+)
+
+module.exports = Donut
