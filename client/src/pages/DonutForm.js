b12bd890edc8fd5573f4bc755d337a41a04ecea4
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-10-25 09:28:50 -0400

    added a handleSubmit to my form

---
+++ client/src/pages/DonutForm.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import e from 'express'
+
+const DonutFrom = () => {
+  let navigate = useNavigate()
+  const initialForm = {
+    name: '',
+    price: '',
+    image: ''
+  }
+
+  const [formState, setFormState] = useState(initialForm)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:3001/donuts', formState)
+    setFormState(initialForm)
+    navigate('/donuts')
+  }
+}
