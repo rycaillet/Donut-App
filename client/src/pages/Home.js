954b81f2a6fa0c68ae3faa8d1a716e6ab71f35f8
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-10-26 10:11:27 -0400

    added some more css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,9 +4,7 @@ const Home = () => {
   return (
     <div className="home-header">
       <h1>Welcome to my Donut app!</h1>
-      <div className="home-donut">
-        {/* <img src="https://i.imgur.com/yTQdd7R.jpeg"></img> */}
-      </div>
+      <div className="home-donut"></div>
     </div>
   )
 }
