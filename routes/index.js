520cc55aa55c6039c1851aa19d7d846405aa5b4a
Ryan Caillet <Ryancaillet1995@gmail.com>
2023-01-10 12:51:02 -0500

    heroku

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 const controllers = require('../controllers')
 const router = Router()
 
-router.get('/', (req, res) => res.send('This is the first page!'))
+// router.get('/', (req, res) => res.send('This is the first page!'))
 
 router.get('/donutShops', controllers.getAllDonutShops)
 
