37090f2b4c05b68b878866200990c86788b815ef
Ryan Caillet <Ryancaillet1995@gmail.com>
2023-01-10 12:44:32 -0500

    heroku

--- db/index.js
+++ db/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-let MONGODB_URI = process.env.MONGODB_URI
-// || 'mongodb://127.0.0.1:27017/donutDatabase'
+let MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/donutDatabase'
 
 mongoose
   .connect(MONGODB_URI)
