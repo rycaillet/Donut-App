7a3db13c2939c1f408f6f2d82af33e1e2797a3f8
Ryan Caillet <Ryancaillet1995@gmail.com>
2023-01-11 11:44:47 -0500

    heroku

--- db/index.js
+++ db/index.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-let MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/donutDatabase'
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/donutDatabase'
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(dbUrl)
   .then(() => {
-    console.log(`Successfully connected to MongoDB at ${MONGODB_URI}`)
+    console.log(`Successfully connected to MongoDB`)
   })
   .catch((e) => {
     console.error('Connection error', e.message)
