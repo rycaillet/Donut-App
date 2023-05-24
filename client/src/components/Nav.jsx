36ed2fa2a057ce625518b79fbfa2fccc967cb60c
Ryan Caillet <Ryancaillet1995@gmail.com>
2023-05-24 14:21:05 -0400

    nav css

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,133 +1,33 @@
-// import { Link } from 'react-router-dom'
-
-// const Nav = () => {
-
-//     return (
-//         <header>
-//             <nav>
-//                 <Link to='/'>Home</Link>
-//                 <Link to='/about'>About</Link>
-//                 <Link to='/donutShops'>Donut Shops</Link>
-//             </nav>
-//         </header>
-//     )
-// }
-
-// export default Nav
-
-// import { useState } from 'react';
-// import { Link } from 'react-router-dom';
-
-// const Nav = () => {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <header>
-//       <nav className={`navbar ${isOpen ? 'open' : ''}`}>
-//         <div className="navbar-brand">
-//           <div className="navbar-toggle" onClick={toggleMenu}>
-//             <div></div>
-//             <div></div>
-//             <div></div>
-//           </div>
-//           <span>Welcome!</span>
-//         </div>
-//         <div className={`navbar-menu ${isOpen ? 'open' : ''}`}>
-//           <div className="navbar-links">
-//             <Link to="/" onClick={toggleMenu}>
-//               Home
-//             </Link>
-//             <Link to="/about" onClick={toggleMenu}>
-//               About
-//             </Link>
-//             <Link to="/donutShops" onClick={toggleMenu}>
-//               Donut Shops
-//             </Link>
-//           </div>
-//         </div>
-//       </nav>
-//       {isOpen && <div className="navbar-overlay" onClick={toggleMenu}></div>}
-//     </header>
-//   );
-// };
-
-// export default Nav;
-
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Nav = () => {
-    const [click, setClick] = useState(false);
-  
-    const handleClick = () => setClick(!click);
-    const Close = () => setClick(false);
-    
-    return (
-      <div>
-       <div className={click ? "main-container" : ""}  onClick={()=>Close()} />
-        <nav className="navbar" onClick={e => e.stopPropagation()}>
-          <div className="nav-container">
-            <Link exact to="/" className="nav-logo">
-              CodeBucks
-              <i className="fa fa-code"></i>
-            </Link>
-            <ul className={click ? "nav-menu active" : "nav-menu"}>
-              <li className="nav-item">
-                <Link
-                  exact
-                  to="/"
-                  activeClassName="active"
-                  className="nav-links"
-                  onClick={click ? handleClick : null}
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  exact
-                  to="/about"
-                  activeClassName="active"
-                  className="nav-links"
-                  onClick={click ? handleClick : null}
-                >
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  exact
-                  to="/blog"
-                  activeClassName="active"
-                  className="nav-links"
-                  onClick={click ? handleClick : null}
-                >
-                  Blog
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  exact
-                  to="/contact"
-                  activeClassName="active"
-                  className="nav-links"
-                 onClick={click ? handleClick : null}
-                >
-                  Contact Us
-                </Link>
-              </li>
-            </ul>
-            <div className="nav-icon" onClick={handleClick}>
-              <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
-            </div>
-          </div>
-        </nav>
-      </ div>
-    );
-  }
+function Nav() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  export default Nav
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <nav>
+      <div className={`mobile-menu-toggle ${isMobileMenuOpen ? 'open' : ''}`} onClick={toggleMobileMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={`menu ${isMobileMenuOpen ? 'open' : ''}`}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/donutShops">Donut Shops</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
