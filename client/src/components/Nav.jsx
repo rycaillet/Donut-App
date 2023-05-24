9f51332f18367565ae9e6c3902e9e15404a77aa2
Ryan Caillet <Ryancaillet1995@gmail.com>
2023-05-24 16:19:34 -0400

    nav css

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -12,18 +12,18 @@ const Nav = () => {
       <div className="navbar-logo">
         Donut Diver
       </div>
+      <div className={`navbar-mobile-toggle ${isMobileMenuOpen ? 'open' : ''}`} onClick={toggleMobileMenu}>
+        <span className="navbar-mobile-toggle-icon"></span>
+      </div>
       <ul className={`navbar-menu ${isMobileMenuOpen ? 'open' : ''}`}>
         <li className="navbar-item">
-          <a href="#" className="navbar-link">Home</a>
+          <a href={`https://donut-app.herokuapp.com/api/home`} className="navbar-link">Home</a>
         </li>
         <li className="navbar-item">
-          <a href="#" className="navbar-link">About</a>
+          <a href={`https://donut-app.herokuapp.com/api/about`} className="navbar-link">About</a>
         </li>
         <li className="navbar-item">
-          <a href="#" className="navbar-link">Services</a>
-        </li>
-        <li className="navbar-item">
-          <a href="#" className="navbar-link">Contact</a>
+          <a href={`https://donut-app.herokuapp.com/api/donutShops`} className="navbar-link">Donut Shops</a>
         </li>
       </ul>
       <div className="navbar-mobile-toggle" onClick={toggleMobileMenu}>
