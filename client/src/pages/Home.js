3756344c1716d8bae6fd757af71f7a355f3b108b
Ryan Caillet <Ryancaillet1995@gmail.com>
2023-05-24 13:38:01 -0400

    nav css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="home-header">
-      <h1>Welcome to my Donut app!</h1>
+      <h1 className="home-h1">Welcome to my Donut app!</h1>
       {/* <div id="donut-picture-1" className="donut-picture"></div> */}
       <img
         className="home-image"
